c43c36e05925dd1c906b97fba70a0c44e93e31c3
Sumandeep2111 <sumandeep21111994@gmail.com>
2020-01-21 13:13:26 -0500

    first commit

---
+++ Sumandeep_c0764942/TaskViewController.swift
@@ -0,0 +1,113 @@
+//
+//  TaskViewController.swift
+//  Sumandeep_c0764942
+//
+//  Created by MacStudent on 2020-01-21.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class TaskViewController: UIViewController {
+
+    var AddTasks:[Taskinfo]?
+    var TaskTable:AddTaskTableViewController?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    loadCoreData()
+        NotificationCenter.default.addObserver(self,selector: #selector(saveCoreData),name: UIApplication.willResignActiveNotification, object: nil)
+        // Do any additional setup after loading the view.
+    }
+    
+    func getFilePath() -> String {
+        let documentPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        if documentPath.count > 0 {
+            let documentDirectory = documentPath[0]
+            let filePath = documentDirectory.appending("/task.txt")
+            return filePath
+        }
+        return ""
+    }
+    
+    @objc func saveCoreData() {
+           // call clear core data
+         //  clearCoreData()
+           // create an instance of app delegate
+           let appDelegate = UIApplication.shared.delegate as! AppDelegate
+           // second step is context
+           let managedContext = appDelegate.persistentContainer.viewContext
+           
+           for task in AddTasks! {
+               let taskEntity = NSEntityDescription.insertNewObject(forEntityName: "TaskEntity", into: managedContext)
+               taskEntity.setValue(task.name, forKey: "name")
+             
+               
+               // save context
+               do {
+                   try managedContext.save()
+               } catch {
+                   print(error)
+               }
+           }
+       }
+    
+    func loadCoreData() {
+        AddTasks = [Taskinfo]()
+        // create an instance of app delegate
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        // second step is context
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "TaskEntity")
+        
+        do {
+            let task = try managedContext.fetch(fetchRequest)
+            if task is [NSManagedObject] {
+                for taskres in task as! [NSManagedObject] {
+                    let name = taskres.value(forKey: "name") as! String
+                    
+                    AddTasks?.append(Taskinfo(name: name))
+                
+                }
+            }
+        } catch {
+            print(error)
+        }
+        
+    }
+    
+    func clearCoreData() {
+        // create an instance of app delegate
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        // second step is context
+        let managedContext = appDelegate.persistentContainer.viewContext
+        // create a fetch request
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "TaskEntity")
+        
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try managedContext.fetch(fetchRequest)
+            for managedObjects in results {
+                if let managedObjectData = managedObjects as? NSManagedObject {
+                    managedContext.delete(managedObjectData)
+                }
+            }
+        } catch {
+            print(error)
+        }
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Sumandeep_c0764942/Taskinfo.swift
@@ -0,0 +1,19 @@
+//
+//  Taskinfo.swift
+//  Sumandeep_c0764942
+//
+//  Created by MacStudent on 2020-01-21.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class Taskinfo {
+    
+    internal init(name: String) {
+        self.name = name
+        
+    }
+    
+    var name:String
+}
